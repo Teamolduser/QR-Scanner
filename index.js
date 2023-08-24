4b498bcc85ebc061fca86cb4c87e25273a6d1200
Teamolduser <120130487+Teamolduser@users.noreply.github.com>
2023-08-24 12:54:04 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
-const makeWASocket = require("@adiwajshing/baileys").default
+const makeWASocket = require("@whiskeysockets/baileys").default
 const qrcode = require("qrcode-terminal")
 const fs = require('fs')
 const pino = require('pino')
-const { delay , useMultiFileAuthState,BufferJSON, fetchLatestBaileysVersion } = require("@adiwajshing/baileys")
+const { delay , useMultiFileAuthState,BufferJSON, fetchLatestBaileysVersion } = require("@whiskeysockets/baileys")
+
+const useStore = !process.argv.includes('--no-store')
+const doReplies = !process.argv.includes('--no-reply')
+const usePairingCode = process.argv.includes('--use-pairing-code')
+const useMobile = process.argv.includes('--mobile')
 
 
   async function qr() {
@@ -11,11 +16,22 @@ let { version, isLatest } = await fetchLatestBaileysVersion()
 const {  state, saveCreds } =await useMultiFileAuthState(`./session`)
     const session = makeWASocket({
         logger: pino({ level: 'silent' }),
-        printQRInTerminal: true,
+        printQRInTerminal: !usePairingCode,
+	    	mobile: useMobile,
         browser: ['Alien-Alfa','opera','1.0.0'],
         auth: state,
         version
     })
+
+    if(usePairingCode) {
+		if(useMobile) {
+			throw new Error('Cannot use pairing code with mobile api')
+		}
+
+		const phoneNumber = '+918602239106'
+		const code = await session.requestPairingCode(phoneNumber)
+		console.log(`Pairing code: ${code}`)
+    }
 //------------------------------------------------------
     session.ev.on("connection.update",async  (s) => {
         const { connection, lastDisconnect } = s
